b356a0341b0fba294896d75a9b2263f3923c58c6
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-01 09:08:43 -0500

    updated import statements.

--- src/vector_db/fill_vectordb.py
+++ src/vector_db/fill_vectordb.py
@@ -1,8 +1,8 @@
 import psycopg2
 from pgvector.psycopg2 import register_vector
-from scripts.ai_integration.openai_embeddings_api import *
-from scripts.server.vector_db.aws_secret import get_secret
-from scripts.server.vector_db.connection_string import connection_string
+from src.ai_integration.openai_embeddings_api import *
+from src.vector_db.aws_sdk_auth import get_secret
+from src.vector_db.aws_database_auth import connection_string
 from other.red import inputRED
 from io import StringIO
 
