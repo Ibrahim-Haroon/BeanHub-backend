3166c41482e6d66f6c7905b59e85e43ee66bd4ed
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-03 07:18:28 -0500

    named app to audio_endpoint and also updated loading aws auth.

--- src/django_beanhub/settings.py
+++ src/django_beanhub/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import pandas as pd
+from os import path
 
 # Load environment variables from .env file
 #env_path = Path(__file__).resolve().parent / '.env'
@@ -44,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     'rest_framework',
-    'src.AudioEndpoint',
+    'src.audio_endpoint',
 ]
 
 MIDDLEWARE = [
@@ -131,14 +133,23 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+secret_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "aws-info.csv")
+df = pd.read_csv(secret_file_path)
+
+row = df.iloc[0]
+
+region_name = row['region_name']
+aws_access_key_id = row['aws_access_key_id']
+aws_secret_access_key = row['aws_secret_access_key']
+
 # AWS S3
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
 # https://codewithmuh.medium.com/configure-aws-s3-with-django-5163abc6dd48
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = aws_access_key_id
+AWS_SECRET_ACCESS_KEY = aws_secret_access_key
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+AWS_S3_REGION_NAME = region_name
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
