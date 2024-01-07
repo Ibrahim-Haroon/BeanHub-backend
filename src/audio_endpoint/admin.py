7009ca988d2a8582b5af285c8455658b86b9aef0
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-07 14:12:42 -0500

    updated list display to reflect new model.

--- src/audio_endpoint/admin.py
+++ src/audio_endpoint/admin.py
@@ -4,9 +4,4 @@ from .models import AudioFile
 
 @admin.register(AudioFile)
 class AudioAdmin(admin.ModelAdmin):
-    list_display = ['display_audio', 'floating_point_number']
-
-    def display_audio(self, obj):
-        return obj.file.url if obj.file else None
-
-    display_audio.short_description = 'Audio File'
+    list_display = ['file_path', 'unique_id', 'json_order']
