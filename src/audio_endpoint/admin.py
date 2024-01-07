cdad97a963f4910f05008aac509407a94c20d546
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-07 14:40:50 -0500

    added more features.

--- src/audio_endpoint/admin.py
+++ src/audio_endpoint/admin.py
@@ -5,3 +5,11 @@ from .models import AudioFile
 @admin.register(AudioFile)
 class AudioAdmin(admin.ModelAdmin):
     list_display = ['file_path', 'unique_id', 'json_order']
+    readonly_fields = ['unique_id']
+    search_fields = ['file_path', 'unique_id']
+    list_filter = ['file_path', 'unique_id']
+
+    def short_json_order(self, obj):
+        return str(obj.json_order)[:50]  # Display the first 50 characters
+
+    short_json_order.short_description = 'JSON Order (shortened)'
