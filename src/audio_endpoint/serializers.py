338aa3fec2d027d2033432b0889ac2ad2b7c8f3e
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-08 09:01:33 -0500

    updated for list of json objects.

--- src/audio_endpoint/serializers.py
+++ src/audio_endpoint/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
+from .models import AudioFile
 
 
-class AudioResponseSerializer(serializers.Serializer):
-    file_path = serializers.CharField()
-    unique_id = serializers.CharField()
-    json_order = serializers.JSONField()
-
+class AudioResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AudioFile
+        fields = ['file_path', 'unique_id', 'json_order']
