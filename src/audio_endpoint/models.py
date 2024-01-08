4a04045af02855e92b6de9a5cb7b341bc7709e9d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-08 18:38:20 -0500

    updated json_order to be a list of json objects.

--- src/audio_endpoint/models.py
+++ src/audio_endpoint/models.py
@@ -5,5 +5,4 @@ import uuid
 class AudioFile(models.Model):
     file_path = models.CharField(max_length=100)
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    json_order = models.JSONField(default=list)
-
+    json_order = models.JSONField()
