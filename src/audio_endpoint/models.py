1d9631896331735d0b14fbaa26b3ceb392cca1de
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-08 08:57:56 -0500

    changed json order to become a list of json objects instead of just one.

--- src/audio_endpoint/models.py
+++ src/audio_endpoint/models.py
@@ -5,5 +5,5 @@ import uuid
 class AudioFile(models.Model):
     file_path = models.CharField(max_length=100)
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
-    json_order = models.JSONField(default=dict)
+    json_order = models.JSONField(default=list)
 
