a9bff289bd3c96142bbd75bb1c39470b72f4f84c
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-08 18:37:58 -0500

    updated for new model which is a list of json objects.

--- src/audio_endpoint/serializers.py
+++ src/audio_endpoint/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import AudioFile
 
 
-class AudioResponseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AudioFile
-        fields = ['file_path', 'unique_id', 'json_order']
+class MenuItemSerializer(serializers.Serializer):
+    item_name = serializers.CharField(max_length=100)
+    quantity = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=5, decimal_places=2)
+    temp = serializers.CharField(max_length=50, required=False)
+    add_ons = serializers.CharField(max_length=100, required=False)
+    milk_type = serializers.CharField(max_length=50, required=False)
+    sweeteners = serializers.CharField(max_length=200, required=False)
+    num_calories = serializers.CharField(max_length=100)
+    cart_action = serializers.CharField(max_length=25)
+
+
+class NestedMenuItemSerializer(serializers.Serializer):
+    MenuItem = MenuItemSerializer()
+
+
+class AudioResponseSerializer(serializers.Serializer):
+    file_path = serializers.CharField(max_length=200)
+    unique_id = serializers.CharField(max_length=100)
+    json_order = serializers.ListField(child=NestedMenuItemSerializer())
+
