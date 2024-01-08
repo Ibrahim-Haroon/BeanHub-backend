2cc8776633ed05824b9449d088d7351126cc67e2
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-08 18:38:52 -0500

    model changed and not worth time to look into updating admin.py right now.

--- src/audio_endpoint/admin.py
+++ src/audio_endpoint/admin.py
@@ -1,15 +1,2 @@
 from django.contrib import admin
 from .models import AudioFile
-
-
-@admin.register(AudioFile)
-class AudioAdmin(admin.ModelAdmin):
-    list_display = ['file_path', 'unique_id', 'json_order']
-    readonly_fields = ['unique_id']
-    search_fields = ['file_path', 'unique_id']
-    list_filter = ['file_path', 'unique_id']
-
-    def short_json_order(self, obj):
-        return str(obj.json_order)[:50]  # Display the first 50 objects
-
-    short_json_order.short_description = 'JSON Order (shortened)'
