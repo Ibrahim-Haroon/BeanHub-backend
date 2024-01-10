b2b09649be5aa6d7d986a1ed6de4fd7f730d8517
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-09 19:50:45 -0500

    changed return type of json and also no longer pass api key but created parser function.

--- src/vector_db/contain_item.py
+++ src/vector_db/contain_item.py
@@ -1,4 +1,5 @@
 import psycopg2
+import json
 from io import StringIO
 from src.vector_db.aws_sdk_auth import get_secret
 from src.ai_integration.openai_embeddings_api import *
@@ -7,20 +8,20 @@ from pgvector.psycopg2 import register_vector
 import numpy as np
 
 
-def contains_quantity(order: str, quantity: int = 1, key: str = None, aws_csv_file: StringIO = None, database_csv_file: StringIO = None) -> bool:
+def contains_quantity(order: str, quantity: int = 1, aws_csv_file: StringIO = None, database_csv_file: StringIO = None):
     """
 
     @rtype: bool
     @param order: customers order ex. "Can I have a black coffee with 3 shots of cream."
     @param quantity:
-    @param key: OpenAI auth
     @param aws_csv_file: AWS SDK auth
     @param database_csv_file: AWS RDS and PostgreSQL auth
     @return: Boolean flag to show whether the item is in stock
     """
     if not order:
-        return None, False
+        return json.dumps(False)
 
+    key = get_openai_key()
     get_secret(aws_csv_file if not None else None)
     db_connection = psycopg2.connect(connection_string(database_csv_file if not None else None))
     db_connection.set_session(autocommit=True)
@@ -39,7 +40,14 @@ def contains_quantity(order: str, quantity: int = 1, key: str = None, aws_csv_fi
     cur.close()
     db_connection.close()
 
-    return True if result[0][2] >= quantity else False
+    return json.dumps([result[0][2] >= quantity, result[0][2]])
+
+
+def get_openai_key(key_path: str = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "api_key.txt")) -> str:
+    with open(key_path) as api_key:
+        key = api_key.readline().strip()
+
+    return key
 
 
 def main() -> int:
