70b92f29cdee326b54bd79fa27574f9ca13613a2
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-13 15:39:04 -0500

    created seperate thread for getting embedding as well as adding vector extension for pg.

--- src/vector_db/get_item.py
+++ src/vector_db/get_item.py
@@ -1,5 +1,6 @@
 import time
 import queue
+import logging
 import psycopg2
 import threading
 import numpy as np
@@ -9,6 +10,8 @@ from pgvector.psycopg2 import register_vector
 from src.vector_db.aws_sdk_auth import get_secret
 from src.ai_integration.openai_embeddings_api import *
 from src.vector_db.aws_database_auth import connection_string
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def get_item(order: str, api_key: str = None, connection_pool=None, database_csv_file: StringIO = None) -> str and bool:
@@ -29,24 +32,24 @@ def get_item(order: str, api_key: str = None, connection_pool=None, database_csv
         db_connection = connection_pool.getconn()
     else:
         db_connection = psycopg2.connect(connection_string(database_csv_file if not None else None))
-    print(f"connection time: {time.time() - connection_time}")
+    logging.info(f"db connection time: {time.time() - connection_time}")
 
     set_session_time = time.time()
     db_connection.set_session(autocommit=True)
-    print(f"set_session time: {time.time() - set_session_time}")
+    logging.info(f"start new db session time: {time.time() - set_session_time}")
 
     return_queue = queue.Queue()
 
     def get_embedding() -> None:
         openai_embedding_time = time.time()
         vector_embedding = openai_embedding_api(order, api_key if api_key else None)
-        print(f"openai_embedding time: {time.time() - openai_embedding_time}")
+        logging.info(f"openai_embedding time: {time.time() - openai_embedding_time}")
         return_queue.put(vector_embedding)
 
     def pg_register_vector() -> None:
         register_vector_time = time.time()
         register_vector(db_connection)
-        print(f"register_vector time: {time.time() - register_vector_time}")
+        logging.info(f"register_vector time: {time.time() - register_vector_time}")
 
     get_embedding_thread = threading.Thread(target=get_embedding)
     pg_register_vector_thread = threading.Thread(target=pg_register_vector)
@@ -54,20 +57,12 @@ def get_item(order: str, api_key: str = None, connection_pool=None, database_csv
     get_embedding_thread.start()
     pg_register_vector_thread.start()
 
-    get_embedding_thread.join()
-    pg_register_vector_thread.join()
-
     cursor_time = time.time()
     cur = db_connection.cursor()
-    print(f"cursor time: {time.time() - cursor_time}")
+    logging.info(f"connecting cursor time: {time.time() - cursor_time}")
 
-    # cur.execute(f"""
-    #                 SELECT id, item_name, item_quantity, common_allergin, num_calories, price
-    #                 FROM products
-    #                 WHERE 1 - (embeddings <=> %s) > {0.8}
-    #                 ORDER BY embeddings ASC
-    #                 LIMIT 1;""",
-    #             (np.array(embedding),))
+    get_embedding_thread.join()
+    pg_register_vector_thread.join()
 
     execute_time = time.time()
     cur.execute(f""" SELECT id, item_name, item_quantity, common_allergin, num_calories, price
@@ -75,22 +70,22 @@ def get_item(order: str, api_key: str = None, connection_pool=None, database_csv
                     ORDER BY embeddings <-> %s 
                     LIMIT 1;""",
                 (np.array(return_queue.get()),))
-    print(f"execute time: {time.time() - execute_time}")
+    logging.info(f"execute query time: {time.time() - execute_time}")
 
     fetchall_time = time.time()
     result = cur.fetchall()
-    print(f"fetchall time: {time.time() - fetchall_time}")
+    logging.info(f"fetchall time: {time.time() - fetchall_time}")
 
     close_time = time.time()
     cur.close()
-    print(f"close time: {time.time() - close_time}")
+    logging.info(f"close cursor time: {time.time() - close_time}")
 
     close_connection_time = time.time()
     if connection_pool:
         connection_pool.putconn(db_connection)
     else:
         db_connection.close()
-    print(f"close_connection time: {time.time() - close_connection_time}")
+    logging.info(f"close db connection time: {time.time() - close_connection_time}")
 
     return result, True
 
@@ -106,12 +101,24 @@ def main() -> int:
 
     total_time = time.time()
     res = get_item(order="cappuccino", api_key=key, connection_pool=connection_pool)
-    print(f"total time: {time.time() - total_time}")
+    logging.info(f"total time: {time.time() - total_time}")
 
-    print(res)
+    logging.info(res)
 
     return 0
 
 
 if __name__ == "__main__":
     main()
+
+
+'''
+If ever change to hnsw index, use this code:
+    cur.execute(f"""
+                    SELECT id, item_name, item_quantity, common_allergin, num_calories, price
+                    FROM products
+                    WHERE 1 - (embeddings <=> %s) > {0.8}
+                    ORDER BY embeddings ASC
+                    LIMIT 1;""",
+                (np.array(embedding),))
+'''
