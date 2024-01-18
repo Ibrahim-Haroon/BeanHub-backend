cbe2d30c34a720305ccbdd65318de4cae1d91649
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-18 11:30:56 -0500

    two test cases to verify app name and default auto field.

--- tests/test_audio_endpoint/test_apps.py
+++ tests/test_audio_endpoint/test_apps.py
@@ -1,6 +1,23 @@
+import pytest
 from django.test import TestCase
+from src.audio_endpoint.apps import AudioEndpointConfig
 
 
-class ModelsTestCase(TestCase):
-    def test_models(self):
-        pass
+@pytest.mark.skip(reason="Need to run with django test not pytest")
+class AudioEndpointConfigTest(TestCase):
+    def test_app_name_is_correct(self):
+        # Arrange
+
+        # Act
+
+        # Assert
+        self.assertEqual(AudioEndpointConfig.name, 'src.audio_endpoint')
+
+    def test_default_auto_field_is_set_correctly(self):
+        # Arrange
+
+        # Act
+
+        # Assert
+        self.assertEqual(AudioEndpointConfig.default_auto_field, 'django.db.models.BigAutoField')
+
