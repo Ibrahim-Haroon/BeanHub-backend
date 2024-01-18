ff04d1cd9421c15145fbc639743e5d44cf26d46d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-17 23:21:59 -0500

    handled case for when database-info.csv DNE. Added loading environmental variables.

--- src/vector_db/aws_database_auth.py
+++ src/vector_db/aws_database_auth.py
@@ -15,20 +15,25 @@ def connection_string(csv_file: StringIO = None) -> str:
     @return: connection string for AWS RDS
     """
     db_info_file_path = path.join(path.dirname(path.realpath(__file__)), "../../other/" + "db_info.csv")
+    try:
+        df = pd.read_csv(db_info_file_path)
+    except FileNotFoundError:
+        df = None
+        pass
 
     if csv_file is None and env('RDS_DB_NAME') and env('RDS_USERNAME') and env('RDS_PASSWORD') and env('RDS_HOSTNAME') and env('RDS_PORT'):
         dsn = f"dbname={env('RDS_DB_NAME')} user={env('RDS_USERNAME')} password={env('RDS_PASSWORD')} host={env('RDS_HOSTNAME')} port={env('RDS_PORT')}"
-        return dsn
-    elif isinstance(csv_file, StringIO):
-        df = pd.read_csv(csv_file)
-    elif db_info_file_path:
-        df = pd.read_csv(db_info_file_path)
     else:
-        raise SystemExit(f"Must either use default csv file path or pass in a csv file, got {type(csv_file)}.")
+        if csv_file is None and not df.empty:
+            pass
+        elif isinstance(csv_file, StringIO):
+            df = pd.read_csv(csv_file)
+        else:
+            raise SystemExit(f"Must either use default csv file path or pass in a csv file, got {type(csv_file)}.")
 
-    row = df.iloc[0]
+        row = df.iloc[0]
 
-    dsn = f"dbname={row['dbname']} user={row['user']} password={row['password']} host={row['host']} port={row['port']}"
+        dsn = f"dbname={row['dbname']} user={row['user']} password={row['password']} host={row['host']} port={row['port']}"
 
     return dsn
 
