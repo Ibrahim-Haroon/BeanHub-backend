b01913f328d84388a5832718ecae67a0311f0a41
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-18 11:27:39 -0500

    don't use model so dummy test.

--- tests/test_audio_endpoint/test_models.py
+++ tests/test_audio_endpoint/test_models.py
@@ -1,6 +1,9 @@
+import pytest
 from django.test import TestCase
 
 
+@pytest.mark.skip(reason="Don't use model so just dummy test")
 class ModelsTestCase(TestCase):
-    def test_models(self):
+    def test_model(self):
         pass
+
