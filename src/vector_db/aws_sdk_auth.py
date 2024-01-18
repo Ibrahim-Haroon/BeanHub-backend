521b6433fd34df739a1f89240805153ccf528a69
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-17 23:22:31 -0500

    handled case for when aws-info.csv DNE. Added loading environmental variables.

--- src/vector_db/aws_sdk_auth.py
+++ src/vector_db/aws_sdk_auth.py
@@ -18,27 +18,32 @@ def get_secret(csv_file: StringIO = None) -> dict:
     @return: ex. {"username":"username","password":"pass","engine":"engine","host":"host","port":5432,"dbname":"name","dbInstanceIdentifier":"db-id"}
     """
     secret_file_path = path.join(path.dirname(path.realpath(__file__)), "../..", "other", "aws-info.csv")
-
-    secret_name, region_name, aws_access_key_id, aws_secret_access_key = "", "", "", ""
+    try:
+        df = pd.read_csv(secret_file_path)
+    except FileNotFoundError:
+        df = None
+        pass
 
     if csv_file is None and env('AWS_ACCESS_KEY_ID') and env('AWS_SECRET_ACCESS_KEY') and env('AWS_DEFAULT_REGION') and env('SECRET_NAME'):
         secret_name = env('SECRET_NAME')
         region_name = env('AWS_DEFAULT_REGION')
         aws_access_key_id = env('AWS_ACCESS_KEY_ID')
         aws_secret_access_key = env('AWS_SECRET_ACCESS_KEY')
-    elif isinstance(csv_file, StringIO):
-        df = pd.read_csv(csv_file)
-    elif secret_file_path:
-        df = pd.read_csv(secret_file_path)
+
+    else:
+        if csv_file is None and not df.empty:
+            pass
+        elif isinstance(csv_file, StringIO):
+            df = pd.read_csv(csv_file)
+        else:
+            raise SystemExit(f"Must either use default csv file path or pass in a csv file, got {type(csv_file)}.")
+
         row = df.iloc[0]
 
         secret_name = row['secret_name']
         region_name = row['region_name']
         aws_access_key_id = row['aws_access_key_id']
         aws_secret_access_key = row['aws_secret_access_key']
-    else:
-        raise SystemExit(f"Must either use default csv file path or pass in a csv file, got {type(csv_file)}.")
-
 
     session = boto3.session.Session()
     client = session.client(
