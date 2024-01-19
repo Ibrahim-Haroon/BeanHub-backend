495ccba660570e7ee62467e91f22b924d9615f7b
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-19 07:56:44 -0500

    updated test to mock redis instance as well.

--- tests/test_vectordb/test_get_item.py
+++ tests/test_vectordb/test_get_item.py
@@ -1,7 +1,8 @@
-import pytest
-from mock import patch, MagicMock
-from io import StringIO
 import csv
+import json
+import pytest
+from io import StringIO
+from mock import MagicMock
 from src.vector_db.get_item import get_item
 
 
@@ -10,8 +11,11 @@ def mock_components(mocker):
     mock_db_instance = mocker.patch('src.vector_db.get_item.psycopg2.connect')
     mock_db_instance.return_value.cursor.return_value.fetchall.return_value = [(7, 'test', 6, 'test', '(60,120)', 10.0)]
 
+    mock_embedding_api = mocker.patch('src.vector_db.get_item.openai_embedding_api')
+    mock_embedding_api.return_value = [0.1, 0.2, 0.3]
+
     return {
-        'openai_embedding_api': mocker.patch('src.vector_db.get_item.openai_embedding_api'),
+        'openai_embedding_api': mock_embedding_api,
         'connection_string': mocker.patch('src.vector_db.aws_database_auth.connection_string'),
     }
 
@@ -30,27 +34,57 @@ def as_csv_file(data: [[str]]) -> StringIO:
     return file_object
 
 
-def test_get_item_returns_true_when_successfully_found_closest_item(mocker, mock_boto3_session_client, mock_components):
+def test_get_item_returns_true_when_successfully_found_closest_item_and_adds_to_cache(
+        mocker, mock_boto3_session_client, mock_components
+):
     # Arrange
-    data = "test"
-    key = "mock_api_key"
+    mock_redis = mocker.Mock()
+    mock_redis.set = MagicMock()
+    mock_redis.exists = MagicMock(return_value=False)
+    mock_redis.get = MagicMock(return_value=json.dumps([0.1, 0.2, 0.3]))
     database_info = [
         ["dbname", "user", "password", "host", "port"],
         ["mydb", "myuser", "mypassword", "localhost", "port"]]
-    aws_info = [
-        ["secret_name", "region_name", "aws_access_key_id", "aws_secret_access_key"],
-        ["name", "us-east-1", "aws_access_key_id", "aws_secret_access_key"]]
-
     db = as_csv_file(database_info)
 
     # Act
-    _, res = get_item(data, api_key=key, database_csv_file=db)
+    _, res = get_item(order="test", api_key="test_key", embedding_cache=mock_redis, database_csv_file=db)
 
     # Assert
     assert res is True, f"expected search to be successful but {res}"
 
+
+def test_get_item_returns_true_when_successfully_found_closest_item_without_being_passed_embedding_cache(
+        mock_boto3_session_client, mock_components
+):
+    # Arrange
+    database_info = [
+        ["dbname", "user", "password", "host", "port"],
+        ["mydb", "myuser", "mypassword", "localhost", "port"]]
+    db = as_csv_file(database_info)
+
+    # Act
+    _, res = get_item(order="test", api_key="test_key", embedding_cache=None, database_csv_file=db)
+
+    # Assert
+    assert res is True, f"expected search to be successful but {res}"
+
+
+def test_get_item_returns_false_when_given_invalid_params(
+        mock_boto3_session_client, mock_components
+):
+    # Arrange
+    data = None
+
+    # Act
+    _, res = get_item(data)
+
+    # Assert
+    assert res is False, f"expected search to be unsuccessful but {res}"
+
+
 #### Temporarily not needed. ###
-# def test_get_item_returns_false_when_given_quantity_greater_than_stock(mocker, mock_boto3_session_client, mock_components):
+# def test_get_item_returns_false_when_given_quantity_greater_than_stock(mock_boto3_session_client, mock_components):
 #     # Arrange
 #     data = "test"
 #     quantity = 1_000
@@ -58,9 +92,6 @@ def test_get_item_returns_true_when_successfully_found_closest_item(mocker, mock
 #     database_info = [
 #         ["dbname", "user", "password", "host", "port"],
 #         ["mydb", "myuser", "mypassword", "localhost", "port"]]
-#     aws_info = [
-#         ["secret_name", "region_name", "aws_access_key_id", "aws_secret_access_key"],
-#         ["name", "us-east-1", "aws_access_key_id", "aws_secret_access_key"]]
 #
 #     aws = as_csv_file(aws_info)
 #     db = as_csv_file(database_info)
@@ -70,14 +101,3 @@ def test_get_item_returns_true_when_successfully_found_closest_item(mocker, mock
 #
 #     # Assert
 #     assert res is False, f"expected search not to return most similar item but {res}"
-
-
-def test_get_item_returns_false_when_given_invalid_params(mocker, mock_boto3_session_client, mock_components):
-    # Arrange
-    data = None
-
-    # Act
-    _, res = get_item(data)
-
-    # Assert
-    assert res is False, f"expected search to be unsuccessful but {res}"
