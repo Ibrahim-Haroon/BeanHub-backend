5976d7bc1889d9d7fea3d3df5948f0d5519b2110
shafaatUML <shafaat.osmani@gmail.com>
2024-01-22 16:20:22 -0500

    implemented swagger API to auto documentation.

--- tests/test_swagger/test_swagger.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase, Client
-
-class SwaggerTestCase(TestCase):
-    def test_swagger_endpoint(self):
-        client = Client()
-        response = client.get('/swagger/?format=openapi')
-        print(response.content)
