a78c33495e52a7582d6bfc5b3d1a141f764dce25
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-24 09:41:55 -0500

    resolved merge conflicts.

--- src/django_beanhub/urls.py
+++ src/django_beanhub/urls.py
@@ -20,8 +20,24 @@ from dotenv import load_dotenv
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 load_dotenv()
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BeanHub API",
+      default_version='v1',
+      description="API documentation for the BeanHub project",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 def root_view(request):
@@ -33,4 +49,6 @@ urlpatterns = [
     path(env('DJANGO_ADMIN_URL', default="admin/"), admin.site.urls),
     path(env('DJANGO_ROOT_URL', default=''), root_view, name='root'),
     path(env('DJANGO_AUDIO_ENDPOINT_URL', default='audio_endpoint/'), include('src.audio_endpoint.urls')),
-]
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
