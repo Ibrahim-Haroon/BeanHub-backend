94fd708c9f5482ae84b6bb6b076d4ce8acc7f68b
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-01-29 19:00:47 -0500

    Revert "added tests for make_order funciton, for all 5 possible cases."
    This reverts commit 0fa0b35508e3077aebdd744f1552332cf0ba9b28.

--- tests/test_ai_integration/test_fine_tuned_nlp.py
+++ tests/test_ai_integration/test_fine_tuned_nlp.py
@@ -46,8 +46,7 @@ def test_that_ner_transformer_returns_prediction_given_string_and_does_not_print
     prediction = ner_transformer("test")
 
     # Assert
-    assert prediction == expected_prediction, \
-        f"expected prediction to be {expected_prediction} but got {prediction}"
+    assert prediction == expected_prediction, f"expected prediction to be {expected_prediction} but got {prediction}"
 
 
 def test_that_ner_transformer_returns_prediction_given_string_and_does_print_out_prediction(
@@ -62,8 +61,7 @@ def test_that_ner_transformer_returns_prediction_given_string_and_does_print_out
 
     # Assert
     assert "[[{'test': 'O'}]]" in captured.out
-    assert prediction == expected_prediction,\
-        f"expected prediction to be {expected_prediction} but got {prediction}"
+    assert prediction == expected_prediction, f"expected prediction to be {expected_prediction} but got {prediction}"
 
 
 def test_that_ner_transformer_returns_empty_list_when_given_empty_string(
@@ -77,204 +75,6 @@ def test_that_ner_transformer_returns_empty_list_when_given_empty_string(
     prediction = ner_transformer(empty_string)
 
     # Assert
-    assert prediction == expected_prediction,\
-        f"expected prediction to be {expected_prediction} but got {prediction}"
+    assert prediction == expected_prediction, f"expected prediction to be {expected_prediction} but got {prediction}"
 
 
-def test_that_make_order_in_Order_class_returns_expected_dict_for_coffee_item(
-        mocker, mock_boto3_session_client, mock_database_components
-) -> None:
-    # Arrange
-    mocker.patch.dict(os.environ, {
-        "AWS_ACCESS_KEY_ID": "test_access_key_id",
-        "AWS_SECRET_ACCESS_KEY": "test_secret_access_key",
-        "AWS_DEFAULT_REGION": "test_region",
-        "SECRET_NAME": "test_secret_name",
-        "RDS_DB_NAME": "test_db_name",
-        "RDS_USERNAME": "test_username",
-        "RDS_PASSWORD": "test_password",
-        "RDS_HOSTNAME": "test_hostname",
-        "RDS_PORT": "test_port"
-    })
-    mock_database_components['connect'].return_value.cursor.return_value.fetchall.return_value = [(7,
-                                                                                                   'test',
-                                                                                                   6,
-                                                                                                   'test',
-                                                                                                   '(60,120)',
-                                                                                                   10.0)]
-    expected_return_value = {
-        'CoffeeItem': {
-            'add_ons': [],
-            'cart_action': 'insertion',
-            'common_allergies_in_item': 'test',
-            'item_name': 'black coffee',
-            'milk_type': 'regular',
-            'num_calories': ['(60,120)'],
-            'price': [10.0],
-            'quantity': [1],
-            'size': 'regular',
-            'sweeteners': [],
-            'temp': 'regular'
-        }
-    }
-    mock_bakery_order = "1 black coffee"
-
-    # Act
-    actual_return_value = Order(mock_bakery_order).make_order()
-
-    # Assert
-    assert actual_return_value == expected_return_value,\
-        f"expected return value to be {expected_return_value} but got {actual_return_value}"
-
-
-def test_that_make_order_in_Order_class_returns_expected_dict_for_beverage_item(
-    mocker, mock_boto3_session_client, mock_database_components
-) -> None:
-    # Arrange
-    mocker.patch.dict(os.environ, {
-        "AWS_ACCESS_KEY_ID": "test_access_key_id",
-        "AWS_SECRET_ACCESS_KEY": "test_secret_access_key",
-        "AWS_DEFAULT_REGION": "test_region",
-        "SECRET_NAME": "test_secret_name",
-        "RDS_DB_NAME": "test_db_name",
-        "RDS_USERNAME": "test_username",
-        "RDS_PASSWORD": "test_password",
-        "RDS_HOSTNAME": "test_hostname",
-        "RDS_PORT": "test_port"
-    })
-    mock_database_components['connect'].return_value.cursor.return_value.fetchall.return_value = [(7,
-                                                                                                   'test',
-                                                                                                   6,
-                                                                                                   'test',
-                                                                                                   '(60,120)',
-                                                                                                   10.0)]
-    expected_return_value = {
-        'BeverageItem': {
-            'add_ons': [],
-            'cart_action': 'insertion',
-            'common_allergies_in_item': 'test',
-            'item_name': 'water',
-            'num_calories': ['(60,120)'],
-            'price': [10.0],
-            'quantity': [1],
-            'size': 'regular',
-            'sweeteners': [],
-            'temp': 'regular'}
-    }
-    mock_bakery_order = "1 water"
-
-    # Act
-    actual_return_value = Order(mock_bakery_order).make_order()
-
-    # Assert
-    assert actual_return_value == expected_return_value,\
-        f"expected return value to be {expected_return_value} but got {actual_return_value}"
-
-
-def test_that_make_order_in_Order_class_returns_expected_dict_for_food_item(
-    mocker, mock_boto3_session_client, mock_database_components
-) -> None:
-    # Arrange
-    mocker.patch.dict(os.environ, {
-        "AWS_ACCESS_KEY_ID": "test_access_key_id",
-        "AWS_SECRET_ACCESS_KEY": "test_secret_access_key",
-        "AWS_DEFAULT_REGION": "test_region",
-        "SECRET_NAME": "test_secret_name",
-        "RDS_DB_NAME": "test_db_name",
-        "RDS_USERNAME": "test_username",
-        "RDS_PASSWORD": "test_password",
-        "RDS_HOSTNAME": "test_hostname",
-        "RDS_PORT": "test_port"
-    })
-    mock_database_components['connect'].return_value.cursor.return_value.fetchall.return_value = [(7,
-                                                                                                   'test',
-                                                                                                   6,
-                                                                                                   'test',
-                                                                                                   '(60,120)',
-                                                                                                   10.0)]
-    expected_return_value = {
-        'FoodItem': {
-            'cart_action': 'insertion',
-            'common_allergies_in_item': 'test',
-            'item_name': 'egg and cheese croissant',
-            'num_calories': ['(60,120)'],
-            'price': [10.0],
-            'quantity': [1]
-        }
-    }
-    mock_bakery_order = "1 egg and cheese croissant"
-
-    # Act
-    actual_return_value = Order(mock_bakery_order).make_order()
-
-    # Assert
-    assert actual_return_value == expected_return_value, \
-        f"expected return value to be {expected_return_value} but got {actual_return_value}"
-
-
-
-def test_that_make_order_in_Order_class_returns_expected_dict_for_bakery_item(
-    mocker, mock_boto3_session_client, mock_database_components
-) -> None:
-    # Arrange
-    mocker.patch.dict(os.environ, {
-        "AWS_ACCESS_KEY_ID": "test_access_key_id",
-        "AWS_SECRET_ACCESS_KEY": "test_secret_access_key",
-        "AWS_DEFAULT_REGION": "test_region",
-        "SECRET_NAME": "test_secret_name",
-        "RDS_DB_NAME": "test_db_name",
-        "RDS_USERNAME": "test_username",
-        "RDS_PASSWORD": "test_password",
-        "RDS_HOSTNAME": "test_hostname",
-        "RDS_PORT": "test_port"
-    })
-    mock_database_components['connect'].return_value.cursor.return_value.fetchall.return_value = [(7,
-                                                                                                   'test',
-                                                                                                   6,
-                                                                                                   'test',
-                                                                                                   '(60,120)',
-                                                                                                   10.0)]
-    expected_return_value = {
-        'BakeryItem': {
-            'cart_action': 'insertion',
-            'common_allergies_in_item': 'test',
-            'item_name': 'glazed donut',
-            'num_calories': ['(60,120)'],
-            'price': [10.0],
-            'quantity': [1]
-        }
-    }
-    mock_bakery_order = "1 glazed donut"
-
-    # Act
-    actual_return_value = Order(mock_bakery_order).make_order()
-
-    # Assert
-    assert actual_return_value == expected_return_value,\
-        f"expected return value to be {expected_return_value} but got {actual_return_value}"
-
-
-def test_that_make_order_in_Order_class_returns_empty_dict_for_item_of_invalid_type(
-    mocker, mock_boto3_session_client, mock_database_components
-) -> None:
-    # Arrange
-    mocker.patch.dict(os.environ, {
-        "AWS_ACCESS_KEY_ID": "test_access_key_id",
-        "AWS_SECRET_ACCESS_KEY": "test_secret_access_key",
-        "AWS_DEFAULT_REGION": "test_region",
-        "SECRET_NAME": "test_secret_name",
-        "RDS_DB_NAME": "test_db_name",
-        "RDS_USERNAME": "test_username",
-        "RDS_PASSWORD": "test_password",
-        "RDS_HOSTNAME": "test_hostname",
-        "RDS_PORT": "test_port"
-    })
-    expected_return_value = {}
-    mock_bakery_order = "1 invalid item"
-
-    # Act
-    actual_return_value = Order(mock_bakery_order).make_order()
-
-    # Assert
-    assert actual_return_value == expected_return_value, \
-        f"expected return value to be {expected_return_value} but got {actual_return_value}"
