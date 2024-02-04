47f5a88023d4a42cf11e6c3646a3a84af553a95d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-04 15:33:23 -0500

    updated  to mock google transcription instead of nova.

--- tests/test_audio_endpoint/test_views.py
+++ tests/test_audio_endpoint/test_views.py
@@ -307,6 +307,70 @@ class AudioEndpointTestCase(TestCase):
         self.assertTrue('unique_id' in response.json())
         self.assertTrue('json_order' in response.json())
 
+    @patch(speech_to_text_path + '.speech.Recognizer')
+    def test_patch_sends_successful_response_when_user_accepts_deal_and_deal_is_coffee_item(
+            self, mock_google_transcribe
+    ) -> None:
+        # Arrange
+        data = {
+            "file_path": "test.wav",
+            "unique_id": "test",
+        }
+
+        mock_deal_data = (
+            '{'
+            '    "deal_accepted": "foo",'
+            '    "deal_object": {'
+            '        "CoffeeItem": {'
+            '            "item_name": "black coffee",'
+            '            "quantity": [1],'
+            '            "price": [2.0],'
+            '            "cart_action": "insertion"'
+            '        }'
+            '    }'
+            '}'
+        )
+
+        self.mock_deal_client.get = MagicMock(return_value=mock_deal_data)
+
+        mock_google_instance = MagicMock()
+        mock_google_transcribe.return_value = mock_google_instance
+        mock_google_transcription = "yes"
+        mock_google_instance.recognize_google.return_value = mock_google_transcription
+
+        # Act
+        response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('file_path' in response.json())
+        self.assertTrue('unique_id' in response.json())
+        self.assertTrue('json_order' in response.json())
+
+    @patch(speech_to_text_path + '.speech.Recognizer')
+    def test_patch_sends_400_error_response_when_user_deal_invalid(
+            self, mock_google_transcribe
+    ) -> None:
+        # Arrange
+        data = {
+            "file_path": "test.wav",
+            "unique_id": "test",
+        }
+
+        mock_google_instance = MagicMock()
+        mock_google_transcribe.return_value = mock_google_instance
+        mock_google_transcription = "yes"
+        mock_google_instance.recognize_google.return_value = mock_google_transcription
+
+        # Act
+        response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
+
+        # Assert
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'error': 'item_type not found'})
+
+    # TODO: Uncomment if revert back to using nova for speech to text (currently using google cloud)
+    '''
     @patch(speech_to_text_path + '.Deepgram')
     def test_patch_sends_successful_response_when_user_accepts_deal_and_deal_is_coffee_item(
             self, mock_deepgram
@@ -352,31 +416,7 @@ class AudioEndpointTestCase(TestCase):
         self.assertTrue('file_path' in response.json())
         self.assertTrue('unique_id' in response.json())
         self.assertTrue('json_order' in response.json())
-
-    @patch(speech_to_text_path + '.speech.Recognizer')
-    def test_patch_sends_400_error_response_when_user_deal_invalid(
-            self, mock_google_transcribe
-    ) -> None:
-        # Arrange
-        data = {
-            "file_path": "test.wav",
-            "unique_id": "test",
-        }
-
-        mock_google_instance = MagicMock()
-        mock_google_transcribe.return_value = mock_google_instance
-        mock_google_transcription = "yes"
-        mock_google_instance.recognize_google.return_value = mock_google_transcription
-
-        # Act
-        response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
-
-        # Assert
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'error': 'item_type not found'})
-
-    # TODO: Uncomment if revert back to using nova for speech to text (currently using google cloud)
-    '''
+        
     @patch(speech_to_text_path + '.Deepgram')
     def test_patch_sends_400_error_response_when_user_deal_invalid(
             self, mock_deepgram
