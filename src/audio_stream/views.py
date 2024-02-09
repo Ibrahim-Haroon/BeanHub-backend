70908405494143fe926a66cbcea7f09eaa55141d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-09 12:46:11 -0500

    added class and function stub.

--- src/audio_stream/views.py
+++ src/audio_stream/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import StreamingHttpResponse
+from confluent_kafka import Consumer, KafkaError
+from src.ai_integration.text_to_speech_api import openai_text_to_speech_api
 
-# Create your views here.
+
+class AudioStreamView(APIView):
+
+    def consume_message(
+            self
+    ):
+        pass
