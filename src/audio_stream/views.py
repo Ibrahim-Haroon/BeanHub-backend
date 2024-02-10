b2c2d7c1f32590b0a70953251fe1bcb8363fe262
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-10 12:00:42 -0500

    integerated redis to share the offset of the first word to reduce time going through empty messages.

--- src/audio_stream/views.py
+++ src/audio_stream/views.py
@@ -1,10 +1,13 @@
+import time
+import json
+import redis
 import logging
 from os import getenv as env
 from dotenv import load_dotenv
 from rest_framework.views import APIView
 from src.django_beanhub.settings import DEBUG
 from django.http import StreamingHttpResponse
-from confluent_kafka import Consumer, KafkaError
+from confluent_kafka import Consumer, KafkaError, TopicPartition, OFFSET_BEGINNING
 from src.ai_integration.text_to_speech_api import openai_text_to_speech_api
 
 logging_level = logging.DEBUG if DEBUG else logging.INFO
@@ -16,50 +19,98 @@ load_dotenv()
 class AudioStreamView(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.consumer = Consumer({
-            'bootstrap.servers': env('KAFKA_BROKER_URL'),
-            'group.id': 'audio_stream_group',
-            'auto.offset.reset': 'earliest'
-        })
-        self.kafka_topic = env('KAFKA_TOPIC')
-        self.consumer.subscribe([self.kafka_topic])
+        self.max_buffer_size = 15
+        self.conversation_cache = self.connect_to_redis_temp_conversation_cache()
+
+    @staticmethod
+    def connect_to_redis_temp_conversation_cache(
+
+    ) -> redis.Redis:  # pragma: no cover
+        while True:
+            try:
+                redis_client = redis.StrictRedis(host=env('REDIS_HOST'), port=env('REDIS_PORT'), db=0)
+                logging.debug("Connected to conversation history")
+                return redis_client
+            except redis.exceptions.ConnectionError:
+                logging.debug("Failed to connect to Redis. Retrying in 5 seconds...")
+                time.sleep(5)
+
+    @staticmethod
+    def on_assign(consumer, partitions):
+        for partition in partitions:
+            partition.offset = OFFSET_BEGINNING
+        consumer.assign(partitions)
+
+    def get_start_offsets(self, unique_id):
+        offset_data = self.conversation_cache.get(f"kafka_message_offset_{unique_id}")
+        if not offset_data:
+            return []
+
+        records = json.loads(offset_data.decode('utf-8'))
+
+        return [TopicPartition(record['topic'], record['partition'], record['offset'] + 1) for record in records]
 
     def consume_message(
             self, unique_id
     ) -> bytes or None:
+        consumer = Consumer({
+            'bootstrap.servers': env('KAFKA_BROKER_URL'),
+            'group.id': 'audio_stream_group',
+            'auto.offset.reset': 'earliest'
+        })
+
+        kafka_topic = env('KAFKA_TOPIC')
+        start_offsets = self.get_start_offsets(unique_id)
+
+        if start_offsets:
+            logging.debug(f"Assigning start offsets: {start_offsets}")
+            consumer.assign(start_offsets)
+        else:
+            logging.debug("No start offsets found. Subscribing to topic...")
+            consumer.subscribe([kafka_topic], on_assign=self.on_assign)
+
         text_buffer = []
-        word_count_target = 4
 
-        while True:
-            msg = self.consumer.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
+        try:
+            while True:
+                msg = consumer.poll(1.0)
+                if msg is None:
+                    logging.debug("No message received")
                     continue
-                elif msg.error().code() != KafkaError.NO_ERROR:
-                    logging.debug(f"Error: {msg.error()}")
-                    break
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        logging.debug(f"End of partition reached {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+                        continue
+                    elif msg.error().code() != KafkaError.NO_ERROR:
+                        logging.debug(f"Error: {msg.error()}")
+                        break
 
-            if msg.value() is None:
-                continue
+                if msg.key() is None or msg.value() is None:
+                    logging.debug("key or value empty. Skipping...")
+                    continue
 
-            if msg.key().decode('utf-8') == unique_id:
-                text = msg.value().decode('utf-8')
+                if msg.key().decode('utf-8') == unique_id:
+                    text = msg.value().decode('utf-8')
+                    logging.debug(f"Received message: {text}")
+                    if text == '!COMPLETE!':
+                        logging.debug("Received completion message.")
+                        if text_buffer:
+                            logging.debug("Buffer not empty. Generating audio...")
+                            audio_bytes = openai_text_to_speech_api(''.join(text_buffer))
+                            yield audio_bytes
+                        break
 
-                # Check for termination signal
-                if text == '!COMPLETE!':
-                    if text_buffer:
+                    text_buffer.append(text)
+                    logging.debug("Appended to buffer")
+
+                    if len(text_buffer) >= self.max_buffer_size:
+                        logging.debug("Buffer full. Generating audio...")
                         audio_bytes = openai_text_to_speech_api(''.join(text_buffer))
                         yield audio_bytes
-                    break
-
-                text_buffer.append(text)
-
-                if len(text_buffer) >= word_count_target:
-                    audio_bytes = openai_text_to_speech_api(''.join(text_buffer))
-                    yield audio_bytes
-                    text_buffer.clear()
+                        text_buffer.clear()
+        finally:
+            consumer.close()
+            logging.debug("Consumer closed")
 
     def get(
             self, request, *args, **kwargs
