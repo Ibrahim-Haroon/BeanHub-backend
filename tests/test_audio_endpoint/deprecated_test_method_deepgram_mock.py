8f44f9e32c894b3776b77f94d4d249f2d3925d4d
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-11 15:48:51 -0500

    added back mock for deepgram.

--- tests/test_audio_endpoint/deprecated_test_method_deepgram_mock.py
+++ tests/test_audio_endpoint/deprecated_test_method_deepgram_mock.py
@@ -15,78 +15,63 @@ nova_response = {
 }
 mock_deepgram_instance.transcription.sync_prerecorded.return_value = nova_response
 
-
-@patch(speech_to_text_path="" + '.Deepgram')
-def test_patch_sends_successful_response_when_user_accepts_deal_and_deal_is_coffee_item(
-        self, mock_deepgram
-) -> None:
-    # Arrange
-    data = {
-        "file_path": "test.wav",
-        "unique_id": "test",
-    }
-
-    mock_deal_data = (
-        '{'
-        '    "deal_accepted": "foo",'
-        '    "deal_object": {'
-        '        "CoffeeItem": {'
-        '            "item_name": "black coffee",'
-        '            "quantity": [1],'
-        '            "price": [2.0],'
-        '            "cart_action": "insertion"'
-        '        }'
-        '    }'
-        '}'
-    )
-
-    self.mock_deal_client.get = MagicMock(return_value=mock_deal_data)
-
-    mock_deepgram_instance = MagicMock()
-    mock_deepgram.return_value = mock_deepgram_instance
-    nova_response = {
-        'results': {
-            'channels': [
-                {'alternatives': [{'transcript': 'yes'}]}
-            ]
+@patch(speech_to_text_path + '.speech.Recognizer')
+    def test_that_patch_returns_200_success_response_when_customer_accepts_deal_and_orders_another_item_in_same_request(
+            self, mock_google_transcribe
+    ) -> None:
+        # Arrange
+        data = {
+            "file_path": "test.wav",
+            "unique_id": "test",
         }
-    }
-    mock_deepgram_instance.transcription.sync_prerecorded.return_value = nova_response
 
-    # Act
-    response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
+        mock_deal_data = (
+            '{'
+            '    "deal_accepted": "foo",'
+            '    "deal_object": {'
+            '        "CoffeeItem": {'
+            '            "item_name": "latte",'
+            '            "quantity": [1],'
+            '            "price": [2.0],'
+            '            "cart_action": "insertion"'
+            '        }'
+            '    }'
+            '}'
+        )
 
-    # Assert
-    self.assertEqual(response.status_code, 200)
-    self.assertTrue('file_path' in response.json())
-    self.assertTrue('unique_id' in response.json())
-    self.assertTrue('json_order' in response.json())
+        mock_google_instance = MagicMock()
+        mock_google_transcribe.return_value = mock_google_instance
+        mock_google_transcription = "yes and one latte please"
+        mock_google_instance.recognize_google.return_value = mock_google_transcription
 
+        self.mock_deal_client.get = MagicMock(side_effect=[mock_deal_data, json.dumps(False)])
 
-@patch(speech_to_text_path="" + '.Deepgram')
-def test_patch_sends_400_error_response_when_user_deal_invalid(
-        self, mock_deepgram
-) -> None:
-    # Arrange
-    data = {
-        "file_path": "test.wav",
-        "unique_id": "test",
-    }
+        # Act
+        response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
 
-    mock_deepgram_instance = MagicMock()
-    mock_deepgram.return_value = mock_deepgram_instance
-    nova_response = {
-        'results': {
-            'channels': [
-                {'alternatives': [{'transcript': 'yes'}]}
-            ]
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('unique_id' in response.json())
+        self.assertTrue('json_order' in response.json())
+
+    @patch(speech_to_text_path + '.speech.Recognizer')
+    def test_patch_sends_400_error_response_when_user_deal_invalid(
+            self, mock_google_transcribe
+    ) -> None:
+        # Arrange
+        data = {
+            "file_path": "test.wav",
+            "unique_id": "test",
         }
-    }
-    mock_deepgram_instance.transcription.sync_prerecorded.return_value = nova_response
 
-    # Act
-    response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
+        mock_google_instance = MagicMock()
+        mock_google_transcribe.return_value = mock_google_instance
+        mock_google_transcription = "yes"
+        mock_google_instance.recognize_google.return_value = mock_google_transcription
 
-    # Assert
-    self.assertEqual(response.status_code, 400)
-    self.assertEqual(response.json(), {'error': 'item_type not found'})
+        # Act
+        response = self.client.patch('/audio_endpoint/', data, content_type='application/json')
+
+        # Assert
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'error': 'item_type not found'})
