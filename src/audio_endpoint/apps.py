d7f535d2dcc3b9cda4f0abb54df7e59fac4e27ba
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-19 17:51:55 -0500

    initilized connections for redis, postgres, aws, and rabbitmq here instead of within view

--- src/audio_endpoint/apps.py
+++ src/audio_endpoint/apps.py
@@ -2,6 +2,16 @@
 This file is used to configure the app name for the audio_endpoint app.
 """
 from django.apps import AppConfig
+from src.vector_db.aws_database_auth import connection_string
+import src.audio_endpoint.redis_connections as redis
+from src.vector_db.aws_sdk_auth import get_secret
+from os import getenv as env
+import boto3
+import psycopg2.pool
+from dotenv import load_dotenv
+from pika import BlockingConnection, ConnectionParameters
+
+load_dotenv()
 
 
 class AudioEndpointConfig(AppConfig):
@@ -10,3 +20,25 @@ class AudioEndpointConfig(AppConfig):
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "src.audio_endpoint"
+
+    def ready(
+            self
+    ) -> None:
+        ####################
+        ## AWS CONNECTION ##
+        get_secret()
+        self.s3 = boto3.client('s3')
+        self.bucket_name = env('S3_BUCKET_NAME')
+        ######################
+        ## REDIS CONNECTION ##
+        self.conversation_cache = redis.connect_to_redis_temp_conversation_cache()
+        self.deal_cache = redis.connect_to_redis_temp_deal_cache()
+        self.embedding_cache = redis.connect_to_redis_embedding_cache()
+        #########################
+        ## RABBITMQ CONNECTION ##
+        self.rabbitmq_connection = BlockingConnection(ConnectionParameters(env('RABBITMQ_HOST')))
+        self.rabbitmq_channel = self.rabbitmq_connection.channel()
+        ###########################
+        ## POSTGRESQL CONNECTION ##
+        self.connection_pool = psycopg2.pool.SimpleConnectionPool(1, 10, connection_string())
+        ###########################
