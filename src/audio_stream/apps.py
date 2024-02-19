db1b913ad35c9d236108cd31d880981648ca6e34
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-19 17:52:28 -0500

    initilized rabbitmq connection here instead of within view

--- src/audio_stream/apps.py
+++ src/audio_stream/apps.py
@@ -2,6 +2,11 @@
 This file is used to configure the app name for the audio_stream app.
 """
 from django.apps import AppConfig
+from os import getenv as env
+from dotenv import load_dotenv
+from pika import BlockingConnection, ConnectionParameters
+
+load_dotenv()
 
 
 class AudioStreamConfig(AppConfig):
@@ -10,3 +15,9 @@ class AudioStreamConfig(AppConfig):
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "src.audio_stream"
+
+    def ready(
+            self
+    ) -> None:
+        self.rabbitmq_connection = BlockingConnection(ConnectionParameters(env('RABBITMQ_HOST')))
+        self.rabbitmq_channel = self.rabbitmq_connection.channel()
