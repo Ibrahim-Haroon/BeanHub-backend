90647280ce8a2128e8a623084fc7c43d3d05ab18
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-02-29 14:01:01 -0500

    decorator to quickly compute time for functions

---
+++ other/decorators/time_log.py
@@ -0,0 +1,23 @@
+"""
+This module contains a decorator that logs the time taken for a function to execute.
+"""
+import time
+import logging
+from src.django_beanhub.settings import DEBUG
+
+LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+logging.basicConfig(level=LOGGING_LEVEL, format='%(asctime)s:%(levelname)s:%(message)s')
+
+
+def time_log(func):
+    """
+    This decorator logs the time taken for a function to execute.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logging.debug(f"{func.__name__} took {end_time - start_time} seconds")
+        return result
+
+    return wrapper
